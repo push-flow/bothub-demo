44686fa2d128b144f15b77adf33c9f963f4e68ab
vctrferreira <victtorferreiralima@gmail.com>
2017-08-11 18:11:42 -0300

    save

--- lib/ralixir_web/controllers/page_controller.ex
+++ lib/ralixir_web/controllers/page_controller.ex
@@ -1,12 +1,29 @@
 defmodule RalixirWeb.PageController do
   use RalixirWeb, :controller
   use Export.Python
+  def start_interpreter do
+    {:ok, py} = Python.start(python_path: Path.expand("utils"))
+    path_models = Path.expand("utils/models/model_20170811-174056")
+    py |> Python.call("caller_rasa", "start_interpreter", [path_models])
+    # IO.puts j
+    r = py |> Python.call("caller_rasa", "interpretator", ["i want food"])
+    IO.puts r
+
+    receive do
+      {from, msg} ->
+        #r = py |> Python.call(interpretator(msg,k ), from_file: "caller_rasa")
+        send from, {self, r}
+    end
+  end
 
   def index(conn, _params) do
-    {:ok, py} = Python.start(python_path: Path.expand("utils"))
-    path_models = Path.expand("utils/models/model_20170810-214535")
-    a = py |> Python.call(get_rasa_response(path_models, "i want eat my brothers"), from_file: "caller_rasa")
-    IO.puts a
-    render conn, "index.html"
+    p = spawn(start_interpreter)
+    send p, {self, "i want eat my brothers"}
+    
+    receive do
+      {from, msg} ->
+        IO.puts msg
+        render conn, "index.html"
+    end
   end
 end
--- utils/caller_rasa.py
+++ utils/caller_rasa.py
@@ -2,9 +2,18 @@ from rasa_nlu.model import Metadata, Interpreter
 from rasa_nlu.config import RasaNLUConfig
 
 import json
+import os
+import pickle
+import dill
 
+OS_PATH = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/models/model_20170811-174056"
 
-def get_rasa_response(path, text):
-    metadata = Metadata.load(path)   # where model_directory points to the folder the model is persisted in
-    interpreter = Interpreter.load(metadata, RasaNLUConfig("/home/victor/projetos/elixir/phoenix/ralixir/utils/config-rasa.json"))
-    return json.dumps(interpreter.parse(unicode(text)))
+def start_interpreter(path):
+    metadata = Metadata.load(MODEL_PATH)
+    interpreter = Interpreter.load(metadata, RasaNLUConfig(OS_PATH+"/config-rasa.json"))
+    return pickle.dumps(interpreter)
+
+def interpretator(msg, interpreter):
+    return pickle.loads(interpreter).parse(unicode(msg))
+

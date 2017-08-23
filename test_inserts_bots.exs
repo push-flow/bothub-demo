d47f0d4a18677d8df7dd57e1000122d904ac8db6
vctrferreira <victtorferreiralima@gmail.com>
2017-08-23 18:28:33 -0300

    change insert tests

--- test_inserts_bots.exs
+++ test_inserts_bots.exs
@@ -5,14 +5,11 @@ list_bots_ids = []
 bot_mananger = %BotManager{host: "localhost", port: 52020}  # setting data to create a bot mananger server
 inserted_bot_mananger = Repo.insert! bot_mananger
 
-bot_instance = %BotInstance{bot_manager: inserted_bot_mananger, model_path: Path.expand("utils/models/model_20170811-174056"), name: "Restaurant Bot"} # setting data to create bot instance in especific bot manager server
+bot_instance = %BotInstance{config_path: Path.expand("utils/config-rasa.json") ,bot_manager: inserted_bot_mananger, model_path: Path.expand("utils/models/model_20170823-204536"), name: "Restaurant Bot"} # setting data to create bot instance in especific bot manager server
 inserted_bot_instance = Repo.insert! bot_instance
-list_bots_ids = [ %{bot_id: inserted_bot_instance.uuid, bot_name: inserted_bot_instance.name, url: "http://localhost:4000/?uuid=#{inserted_bot_instance.uuid}&msg=msg here"} | list_bots_ids]
+list_bots_ids = [ %{bot_id: inserted_bot_instance.uuid, bot_name: inserted_bot_instance.name, url: ":4000/?uuid=#{inserted_bot_instance.uuid}&msg=msg here"} | list_bots_ids]
 
-bot_instance = %BotInstance{bot_manager: inserted_bot_mananger, model_path: Path.expand("utils/models/model_20170816-095626"), name: "Bot Zika En"}
+bot_instance = %BotInstance{config_path: Path.expand("utils/config-rasa.zika.en.json") ,bot_manager: inserted_bot_mananger, model_path: Path.expand("utils/models/model_20170823-204504"), name: "zika Bot"} # setting data to create bot instance in especific bot manager server
 inserted_bot_instance = Repo.insert! bot_instance
-list_bots_ids = [ %{bot_id: inserted_bot_instance.uuid, bot_name: inserted_bot_instance.name, url: "http://localhost:4000/?uuid=#{inserted_bot_instance.uuid}&msg=msg here"} | list_bots_ids]
+list_bots_ids = [ %{bot_id: inserted_bot_instance.uuid, bot_name: inserted_bot_instance.name, url: ":4000/?uuid=#{inserted_bot_instance.uuid}&msg=msg here"} | list_bots_ids]
 
-bot_instance = %BotInstance{bot_manager: inserted_bot_mananger, model_path: Path.expand("utils/models/model_20170816-102954"), name: "Bot Zika Fr"}
-inserted_bot_instance = Repo.insert! bot_instance
-list_bots_ids = [ %{bot_id: inserted_bot_instance.uuid, bot_name: inserted_bot_instance.name, url: "http://localhost:4000/?uuid=#{inserted_bot_instance.uuid}&msg=msg here"} | list_bots_ids]

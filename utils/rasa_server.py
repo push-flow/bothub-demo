707cf273987b37d655d4cc29ddd3a0ca51006238
vctrferreira <victtorferreiralima@gmail.com>
2017-08-25 10:02:17 -0300

    change bot manager

--- utils/rasa_server.py
+++ utils/rasa_server.py
@@ -1,31 +1,94 @@
 from socket import *
-from multiprocessing import Process
-from caller_rasa import start_new_bot
+from rasabot import RasaBot
+from rasabot import RasaBotProcess
+from multiprocessing import Queue
+from multiprocessing import Event
 
 import time
 import json
+
+
+class BotManager():
+    '''
+    javascript client:
+    var ws = new WebSocket('ws://localhost:8888/ws');
+    ws.onmessage = (evt) => {
+        console.log(JSON.parse(evt.data));
+    }
+    var bot_message = {
+        question: 'Whos there?',
+        botId: '123456'
+    }
+    ws.send(JSON.stringify(bot_message))
+    '''
+    _pool = {}
+
+    def _get_bot_data(self, bot_id):
+        bot_data = {}
+        if bot_id in self._pool:
+            print('Reusing an instance...')
+            bot_data = self._pool[bot_id]
+        else:
+            print('Creating a new instance...')
+            ####################### V1
+            # bot = RasaBot('../etc/spacy/config.json')
+            # bot.trainning('../etc/spacy/data/demo-rasa.json', '../etc/spacy/models/')
+            ####################### V2
+            rasa_config = 'config-rasa.zika.en.json'
+            model_dir = 'models/'
+            data_file = 'zika_en.json'
+            answers_queue = Queue()
+            questions_queue = Queue()
+            new_question_event = Event()
+            new_answer_event = Event()
+            bot = RasaBotProcess(questions_queue, answers_queue, new_question_event, new_answer_event, rasa_config, model_dir, data_file)
+            bot.daemon = True
+            bot.start()
+            bot_data['bot_instance'] = bot
+            bot_data['answers_queue'] = answers_queue
+            bot_data['questions_queue'] = questions_queue
+            bot_data['new_question_event'] = new_question_event
+            bot_data['new_answer_event'] = new_answer_event
+            self._pool[bot_id] = bot_data
+        return bot_data
+
+    def _get_new_answer_event(self, bot_id):
+        return self._get_bot_data(bot_id)['new_answer_event']
+
+    def _get_new_question_event(self, bot_id):
+        return self._get_bot_data(bot_id)['new_question_event']
+
+    def _get_questions_queue(self, bot_id):
+        return self._get_bot_data(bot_id)['questions_queue']
+
+    def _get_answers_queue(self, bot_id):
+        return self._get_bot_data(bot_id)['answers_queue']
+
+    def ask(self, question, bot_id):
+        questions_queue = self._get_questions_queue(bot_id)
+        answers_queue = self._get_answers_queue(bot_id)
+        questions_queue.put(question)
+        new_question_event = self._get_new_question_event(bot_id)
+        new_question_event.set()
+        # Wait for answer...
+        # This is not the best aproach! But works for now. ;)
+        # while answers_queue.empty():
+        #     time.sleep(0.001)
+        new_answer_event = self._get_new_answer_event(bot_id)
+        new_answer_event.wait()
+        new_answer_event.clear()
+        return answers_queue.get()
+
+    def start_bot_process(self, bot_id):
+        self._get_questions_queue(bot_id)
 
 
 host = 'localhost'
 port = 52020
 address = host, port
 
-bots = {}
-
-def send_msg_to_bot(uuid, msg):
-    import socket
-    address = "/tmp/bothub-%s.sock" % uuid
-    client = socket.socket(socket.AF_UNIX)
-
-    client.connect(address)
-    client.send(msg.encode())
-    retorno = client.recv(2048)
-    client.close()
-
-    return retorno
- 
-
 with socket() as sock:
+    bm = BotManager()
     sock.bind(address)
     sock.listen()
 
@@ -34,39 +97,23 @@ with socket() as sock:
         print(1)
         data = conn.recv(1024)
         data = json.loads(data.decode('utf_8'))
+
+        uuid = data.get("uuid", None)
+        bm.start_bot_process(uuid)
+
         model_path = data.get("model_path", None)
         config_path = data.get("config_path", None)
         msg = data.get("msg", None)
-        uuid = data.get("uuid", None)
 
         if model_path and msg:
             print(2)
-            if bots.get(uuid, None):
-                print(3)
-                print(bots.get(uuid, None))
-                try:
-                    conn.send(send_msg_to_bot(uuid, msg))
-                    conn.close()
-                except:
-                    conn.close()
-
-            else:
-                print(4)
-                p = Process(target=start_new_bot, args=(uuid, model_path, config_path, ))
-                p.start()
-                bots.update({uuid: {
-                    "model_path": model_path,
-                    "config_path": config_path,
-                    "pid": p.pid
-                }})
-                time.sleep(60)
-                if bots.get(uuid, None):
-                    print(5)
-                    try:
-                        conn.send(send_msg_to_bot(uuid, msg))
-                        conn.close()
-                    except:
-                        print("except on send post create bot")
-                        conn.close()
+            answer = bm.ask(msg, uuid)
+            answer_data = {
+                'botId': uuid,
+                'answer': answer
+            }
+            conn.send(json.dumps(answer_data).encode())
+            conn.close()
         else:
-            pass
+            print(3)
+            conn.close()
---
+++ utils/rasabot.py
@@ -0,0 +1,148 @@
+import time
+from hashlib import sha1
+from multiprocessing import Process
+from rasa_nlu.converters import load_data
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Trainer
+from rasa_nlu.model import Metadata, Interpreter
+from unipath import Path
+
+
+def calc_hash(filename):
+    BUF_SIZE = 65536
+    hash_sha1 = sha1()
+    with open(filename, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            hash_sha1.update(data)
+    return hash_sha1.hexdigest()
+
+
+class RasaBot():
+    '''
+    Before trainning your bot, make sure do you have the last model lang file.
+
+    python -m spacy download en
+    '''
+    def __init__(self, config_file):
+        # self.data_file = '../etc/spacy/data/demo-rasa.json'
+        # self.config_file = '../etc/spacy/config.json'
+        # self.model_dir = '../etc/spacy/models/'
+        self.config_file = config_file
+        self.interpreter = None
+
+    # def _data_file_modified(self, data_file):
+    #     data_file_hash = hash_file(data_file)
+    #     with open('')
+
+    def ask(self, question):
+        if self.interpreter is None:
+            metadata = Metadata.load(self.model_directory)   # where model_directory points to the folder the model is persisted in
+            self.interpreter = Interpreter.load(metadata, RasaNLUConfig(self.config_file))
+        return self.interpreter.parse(question)
+
+    def trainning(self, data_file, model_dir, force=False):
+        '''
+        Creates a new trainning for the bot. This method only makes a new training if the .trainning_hash file does not
+        exist, or, if the data_file hash is changed.
+
+        When force is True we force a new trainning. Even though there is already one for the current data file.
+        '''
+        model_dir = Path(model_dir)
+        data_file = Path(data_file)
+        trainning_hash_file = model_dir.child('.trainning_hash')
+        new_trainning = True
+
+        # The data_file has been modified?
+        if trainning_hash_file.exists() and not force:
+            new_trainning_hash = calc_hash(data_file)
+            current_trainning_hash = trainning_hash_file.read_file()
+            new_trainning = new_trainning_hash != current_trainning_hash
+
+        if new_trainning or force:
+            training_data = load_data(data_file)
+            trainer = Trainer(RasaNLUConfig(self.config_file))
+            trainer.train(training_data)
+            trainer.persist(model_dir)  # Returns the directory the model is stored in
+            trainning_hash_file.write_file(calc_hash(data_file))
+
+    @property
+    def model_directory(self):
+        return 'models/model_20170825-095340'
+
+
+class RasaBotV2():
+    '''
+    Before trainning your bot, make sure do you have the last model lang file.
+
+    python -m spacy download en
+
+    This version load interpreter on initialization.
+    '''
+    def __init__(self, config_file, model_dir, data_file):
+        self.data_file = data_file
+        self.model_dir = model_dir
+        self.config_file = config_file
+        metadata = Metadata.load(self.model_directory)   # where model_directory points to the folder the model is persisted in
+        self.interpreter = Interpreter.load(metadata, RasaNLUConfig(self.config_file))
+
+    def ask(self, question):
+        return self.interpreter.parse(question)
+
+    def trainning(self, force=False):
+        '''
+        Creates a new trainning for the bot. This method only makes a new training if the .trainning_hash file does not
+        exist, or, if the data_file hash is changed.
+
+        When force is True we force a new trainning. Even though there is already one for the current data file.
+        '''
+        model_dir = Path(self.model_dir)
+        data_file = Path(self.data_file)
+        trainning_hash_file = model_dir.child('.trainning_hash')
+        new_trainning = True
+
+        # The data_file has been modified?
+        if trainning_hash_file.exists() and not force:
+            new_trainning_hash = calc_hash(data_file)
+            current_trainning_hash = trainning_hash_file.read_file()
+            new_trainning = new_trainning_hash != current_trainning_hash
+
+        if new_trainning or force:
+            training_data = load_data(data_file)
+            trainer = Trainer(RasaNLUConfig(self.config_file))
+            trainer.train(training_data)
+            trainer.persist(model_dir)  # Returns the directory the model is stored in
+            trainning_hash_file.write_file(calc_hash(data_file))
+
+    @property
+    def model_directory(self):
+        return 'models/model_20170825-095340'
+
+
+class RasaBotProcess(Process):
+    def __init__(self, questions_queue, answers_queue, new_question_event, new_answer_event, rasa_config, model_dir, data_file, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._bot = None
+        self.questions_queue = questions_queue
+        self.answers_queue = answers_queue
+        self.new_question_event = new_question_event
+        self.new_answer_event = new_answer_event
+        self.rasa_config = rasa_config
+        self.model_dir = model_dir
+        self.data_file = data_file
+
+    def run(self, *args, **kwargs):
+        print('run')
+        self._bot = RasaBotV2(self.rasa_config, self.model_dir, self.data_file)
+        while True:
+            # This is not the best aproach! But works for now. ;)
+            # while self.questions_queue.empty():
+            #     time.sleep(0.001)
+            self.new_question_event.wait()
+            self.new_question_event.clear()
+            print('A new question arrived!')
+            answer = self._bot.ask(self.questions_queue.get())
+            self.answers_queue.put(answer)
+            self.new_answer_event.set()
